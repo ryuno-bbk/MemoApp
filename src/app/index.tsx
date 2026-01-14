403b8ed421636598ad01614655beb32a1da7c430
ryuno <ryunosuke20051102@icloud.com>
2026-01-14 19:51:22 +0900

    Styling MemoList

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,44 +11,34 @@ const Index = () => {
                 </View>
             </View>
 
-            <View>
-                <View>
+            <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
-                </View>
             </View>
 
-            <View>
-                <View>
+            <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
-                </View>
             </View>
 
-            <View>
-                <View>
+            <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
-                </View>
-            </View>
-
-            <View>
-                <Text>+</Text>
             </View>
 
         </View>
@@ -80,6 +70,25 @@ const styles = StyleSheet.create({
         lineHeight: 32,
         fontWeight: 'bold',
         color: '#ffffff'
+    },
+    memoListItem: {
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',              // flexBoxを横に。初期では縦
+        justifyContent: 'space-between',   // 二つの要素の間にスペース
+        paddingVertical: 16,               // 上下の余白padding
+        paddingHorizontal: 19,             // 左右の余白padding
+        alignItems:'center',               // Xを真ん中に
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0, 0, 0, 0.15)'
+    },
+    memoListItemTitle: {
+        fontSize: 16,
+        lineHeight: 32
+    },
+    memoListItemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
     }
 })
 
