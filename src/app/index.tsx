d5ea1806aca69f21aef66543ca3e197a92d3ca5e
ryuno <ryunosuke20051102@icloud.com>
2026-01-14 19:22:34 +0900

    Styling Header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,12 @@ import { View, Text, StyleSheet } from 'react-native'
 
 const Index = () => {
     return (
-        <View style= {styles.container}>
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+        <View style={styles.container}>
+
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
 
@@ -57,9 +58,30 @@ const Index = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center', // 縦
-        alignItems: 'center'        // 横
+        backgroundColor: '#ffffff'
+    },
+    header: {
+        backgroundColor: '#467FD3',
+        height: 104,
+        justifyContent: 'flex-end'
+    },
+    headerInner: {
+        alignItems: 'center'
+    },
+    headerRight: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255, 255, 255, 0.7)'
+    },
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
     }
 })
+
 
 export default Index
