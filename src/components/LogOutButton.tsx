96003ca8555ae28cb1c45ce7c7dcd79437b45095
ryuno <ryunosuke20051102@icloud.com>
2026-01-18 01:13:30 +0900

    Firebase LogOut

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,8 +1,22 @@
-import { TouchableOpacity, Text, StyleSheet } from "react-native"
+import { signOut } from "firebase/auth"
+import { TouchableOpacity, Text, StyleSheet, Alert } from "react-native"
+import { router } from "expo-router"
+
+import { auth } from "../config"
+
+const handlePress = (): void => {
+    signOut(auth)
+    .then(() => {
+        router.replace('/auth/log_in')
+    })
+    .catch(() => {
+        Alert.alert('ログアウトに失敗しました')
+    })
+}
 
 const LogOutButton = () => {
     return (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handlePress}>
             <Text style={styles.text}>ログアウト</Text>
         </TouchableOpacity>
     )
