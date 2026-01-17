e07bb1b79c4e200fdf4c61a710ed20d6bc3f2f85
ryuno <ryunosuke20051102@icloud.com>
2026-01-17 14:28:45 +0900

    Stack LogOutBottun

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -8,7 +8,7 @@ import Button from '../../components/button'
 
 const handlePress = (): void => {
  // ログイン
-  router.push('/memo/list')
+  router.replace('/memo/list')
 }
 
 const LogIn = () => {
--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,15 +1,24 @@
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { router, useNavigation } from 'expo-router'
+import { useEffect } from 'react'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
-import { router } from 'expo-router'
+import LogOutButton from '../../components/LogOutButton'
 
 const handlePress = (): void => {
     router.push('/memo/create')
 }
 
 const Index = () => {
+    const navigation = useNavigation()
+    useEffect( () => {
+        navigation.setOptions({
+            headerRight: () => { return <LogOutButton /> }
+    })
+    }, [])
+
     return (
         <View style={styles.container}>
             <View>
