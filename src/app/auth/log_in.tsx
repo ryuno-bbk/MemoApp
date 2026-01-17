bb4e1e690b762512a3b30414c183a7acb9864e03
ryuno <ryunosuke20051102@icloud.com>
2026-01-17 14:42:26 +0900

    useState Text Input

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,8 +1,8 @@
 import {
      View, Text, StyleSheet, TextInput, TouchableOpacity
 } from 'react-native'
-
 import { Link, router} from 'expo-router'
+import { useState } from 'react'
 
 import Button from '../../components/button'
 
@@ -12,13 +12,23 @@ const handlePress = (): void => {
 }
 
 const LogIn = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] =useState('')
     return (
         <View style={styles.container}>
 
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
-                <TextInput style={styles.input} value='Email addres' />
-                <TextInput style={styles.input} value='Password' />
+                <TextInput
+                  style={styles.input}
+                  value={email}
+                  onChangeText={(text) => ( setEmail(text) )}
+                  />
+                <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={(text) => ( setPassword(text) )}
+                />
 
                 <Button Label='Submit' onPress={handlePress}/>
 
